b4d2ff72ba8cb0d5c76f97d24c32494e52b50958
cg4mes <167551389+cg4mes@users.noreply.github.com>
2025-12-13 15:56:27 -0500

    Delete directory

--- src/constants/icons.ts
+++
@@ -1,8 +0,0 @@
-export const ICON_PATHS = {
-  folder: 'M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z',
-  checkCircle: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
-  lightning: 'M13 10V3L4 14h7v7l9-11h-7z',
-  chartBar: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
-  plus: 'M12 6v6m0 0v6m0-6h6m-6 0H6',
-  arrowLeft: 'M15 19l-7-7 7-7',
-} as const;
